e7b04e1ca8f113e0a502dde72f227b28c5440092
Rohan-R07 <rveda013@gmail.com>
2026-02-01 07:07:05 +0530

    Cpp-commit-1

---
+++ Cpp/program-01.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+     //4 bytes
+    int n;
+    cout<<"Enter size";
+    cin>>n;
+int A[n];
+A[0]=2;
+  for(int x:A){
+    cout<<x<<endl;
+  }
+   
+
+    return 0;
+}
